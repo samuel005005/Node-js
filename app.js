0d50fdfcc2060d63bcd95b219e0ae095af42bfa8
Samuel Paez <spaez@viva.com.do>
2021-11-25 17:14:51 -0400

    hbs

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require('express');
-const app = express();
-const path = require('path')
-const port = 8080;
+const hbs = require('hbs');
+const path = require('path');
 const public = path.join(__dirname,'public');
+const app = express();
+const port = 8080;
+
+
+/**
+ * Handlebars   
+ */
+app.set('view engine','hbs');
+hbs.registerPartials( __dirname + '/views/partials',  (err) => {});
+
 /**
  * Servir contenido estatico
  */
 app.use(express.static(public));
 
+const data = {
+    title : "Curso de node js",
+    name : "Samuel Paez Perez"
+};
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('home',data);
 });
 
+app.get('/generic', function (req, res) {
+    res.render('generic',data);
+});
+
+app.get('/elements', function (req, res) {
+    res.render('elements',data);
+});
 
 app.get('*', function (req, res) {
     res.sendFile(`${public}/404.html`,404);
