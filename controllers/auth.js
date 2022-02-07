b09037100355447288e6fab0e1f53c576018f271
Samuel Paez <spaez@viva.com.do>
2022-02-06 23:37:40 -0400

    auth validate user

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,39 @@ const { response } = require("express")
 
 const login = (req , res = response) => {
 
-    res.json({
-        "msg":"Login"
-    })
+    const { correo, password } = req.body;
+    const Usuario = require('../models/user');
+
+    try {
+
+        // Validar si existe el email
+        const usuario = Usuario.findOne({ correo });
+        if( !usuario ){
+            return res.status(400).json({
+                msg: "Usuario o Password incorrecto"
+            });
+        }
+
+        // Validar Si el usuario esta activo
+
+
+        // Validar la contrasena
+
+
+        // Generar el JWT
+
+
+        res.json({
+            "msg":"Login"
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            "msg":"Hable con el administrador",
+        }); 
+    }
+
 }
 
 
