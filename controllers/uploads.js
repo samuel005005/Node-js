f64893d1be99a5db5c4034e8df0ac18f5335f12e
Samuel Paez <spaez@viva.com.do>
2022-02-21 15:19:48 -0400

    Subidas mutiples archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const path = require('path');
+const { obtenerExtensionArchivo, ExtensionNoPermitida } = require('../helpers');
 
 const cargarArchivos = async (req = request, res = response) => {
 
@@ -18,43 +19,77 @@ const cargarArchivos = async (req = request, res = response) => {
     const basePath = path.join(__dirname, `../uploads/`);
 
     const { archivos } = req.files;
-    
+
+
+     /** Validar extension */
+     const extensionesValidas = ['jpg','jpeg','csv','json'];
     /**
      * Si es un solo archivo
      */
      if ( !Array.isArray(archivos) ){
 
+         const  msg =   obtenerExtensionArchivo(archivos.name, extensionesValidas);
+         if( msg ){
+            return res.status(400).json({ msg });
+         }
+
         const uploadPath = `${basePath}${archivos.name}`
 
         archivos.mv(uploadPath, (error) => {
+    
+            if(error){
+                return res.status(500).json({error});
+            }
+    
+        });
+
+        return  res.json({
+            msg: "Subido correctamente"
+        });
+
+    } else {
+
+        try {
+            archivos.map( 
+                (archivo) => {
+                    const  msg =   obtenerExtensionArchivo(archivo.name, extensionesValidas);
+                    if( msg ){ 
+                        throw new ExtensionNoPermitida(msg)
+                    } else {
+                        const uploadPath =  `${basePath}${archivo.name}`;
+    
+                        archivo.mv(uploadPath, (error) => {
+                            if(error){
+                                throw new Error(error)
+                            }
+                        }); 
+                    }
+                }
+            );  
+
+            res.json({
+                msg: "Subidos correctamente"
+            });
+
+        } catch (error) {
+
+            if( error instanceof ExtensionNoPermitida){
+                return res.status(400).json({
+                    msg: error.message
+                });
+            }else {
+                res.status(500).json({
+                    msg: error.message
+                });
+            }
             
-            if(error){
-                return res.status(500).json({error});
-            }
+        }
+
+
     
-            return res.json({
-                msg: "Subido correctamente"
-            })
-        });
+       
+    }
 
-     }
-
-     archivos.forEach(archivo => {
-
-        const uploadPath =  `${basePath}${archivo.name}`;
-
-        archivo.mv(uploadPath, (error) => {
-            if(error){
-                return res.status(500).json({error});
-            }
-    
-        });
-     });
-
-     return res.json({
-        msg: "Subidos correctamente"
-    });
-   
 }
 
 
--- helpers/util.js
+++ helpers/util.js
@@ -1,8 +1,32 @@
-function capitalizeWords(string) {
+const { response } = require("express");
+
+const capitalizeWords = (string) => {
     return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
 };
 
+const obtenerExtensionArchivo = (nombreArchivo = '', extensionesValidas = []) => {
+
+    const nombreCortado = nombreArchivo.split('.');
+    const extension = nombreCortado[ nombreCortado.length - 1];
+
+
+    if( !extensionesValidas.includes(extension)) {
+        return `La extension ${extension} no es permitida`;
+    }
+ 
+}
+
+
+class ExtensionNoPermitida extends Error {
+
+    constructor(message) {
+      super(message);
+      this.name = "ExtensionNoPermitida";
+    }
+}
 
 module.exports = {
-    capitalizeWords
+    capitalizeWords,
+    obtenerExtensionArchivo,
+    ExtensionNoPermitida
 }
